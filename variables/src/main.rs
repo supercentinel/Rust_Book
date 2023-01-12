6a2e6343bcc8fcaa0014d8dc894f678b1b64e6fc
AlaN <digitalforce.ac@gmail.com>
2023-01-11 18:03:38 -0600

    variables completed

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -17,4 +17,17 @@ fn main() {
     let spaces = spaces.len();
     print!("El numero de espacios es {spaces}");
 
+    let _guess: u32 = "42".parse().expect("no es un numero");
+
+    let _int_tuple: (i8, u8,
+                    i16, u16,
+                    i32, u32,
+                    i64, u64,
+                    i128, u128,
+                    isize, usize) = (-12, b'F', -0x13, 0x1c, -0o12, 0o34, -0b1101, 0b1101110, -1234124, 123124, 345609346, 346373890);
+
+    let _f32_array: [f32; 7];
+    let _f64_array: [f64; 8];
+
+
 }
